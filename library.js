caaaf302254546e8e023ce356400d8f8db4e4b56
a5mith <aaronsmith91@sky.com>
2014-06-17 21:29:19 +0100

    Oops

--- library.js
+++ library.js
@@ -2,7 +2,7 @@
     "use strict";
 
     var twitchtv = {},
-        embed = '<iframe frameborder="0" scrolling="no" src="http://twitch.tv/$1/chat?popout=" height="500" width="350"></iframe>';
+        embed = '    <object type="application/x-shockwave-flash" height="378" width="620" id="live_embed_player_flash" data="http://www.twitch.tv/widgets/live_embed_player.swf?channel=$1" bgcolor="#000000"><param name="allowFullScreen" value="true" /><param name="allowScriptAccess" value="always" /><param name="allowNetworking" value="all" /><param name="movie" value="http://www.twitch.tv/widgets/live_embed_player.swf" /><param name="flashvars" value="hostname=www.twitch.tv&channel=$1&auto_play=false&start_volume=50" /></object>';
 
 
     twitchtv.parse = function(postContent, callback) {
