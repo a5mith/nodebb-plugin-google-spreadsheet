6f2db58e221225c17fac049b142251b61226deab
a5mith <aaronsmith91@sky.com>
2014-06-17 21:22:15 +0100

    0.0.1
    Twitching from all this coffee...

---
+++ README.md
@@ -0,0 +1,13 @@
+# NodeBB TwitchTV Plugin
+
+This NodeBB plugin allows users to embed TwitchTV streams inline in posts.
+
+## Installation
+
+    npm install nodebb-plugin-twitchtv
+
+## Usage
+
+    Paste the URL into a post, the system will do the rest.
+    ???
+    Profit!
---
+++ library.js
@@ -0,0 +1,14 @@
+(function(module) {
+    "use strict";
+
+    var twitchtv = {},
+        embed = '<iframe frameborder="0" scrolling="no" src="http://twitch.tv/$1/chat?popout=" height="500" width="350"></iframe>';
+
+
+    twitchtv.parse = function(postContent, callback) {
+        postContent = postContent.replace(/<a href="(?:http?:\/\/)?(?:twitch\.tv)\/?(.+)">.+<\/a>/g, embed);
+        callback(null, postContent);
+    };
+
+    module.exports = twitchtv;
+}(module));
