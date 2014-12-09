72d18e6d8764dcf6d004bae4ea3171465ffa09c6
a5mith <aaronsmith91@sky.com>
2014-12-09 19:23:32 +0000

    0.0.1
    First Sweep, Should work fine...

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
-# NodeBB TwitchTV Plugin
+# NodeBB Google Spreadsheet Plugin
 
-This NodeBB plugin allows users to embed TwitchTV streams inline in posts.
+This NodeBB plugin allows users to embed Spreadsheets inline in posts.
 
 ## Installation
 
-    npm install nodebb-plugin-twitchtv
+    npm install nodebb-plugin-google-spreadsheet
 
 ## Usage
 
     Paste the URL into a post, the system will do the rest.
     ???
     Profit!
+
+To Do:
+
+Add support for word and all that other good stuff.
+
+Support for 0.6 of NodeBB. Currently only built this for a user on the forums.
--- library.js
+++ library.js
@@ -1,14 +1,14 @@
 (function(module) {
     "use strict";
 
-    var twitchtv = {},
-        embed = '    <object type="application/x-shockwave-flash" height="378" width="620" id="live_embed_player_flash" data="http://www.twitch.tv/widgets/live_embed_player.swf?channel=$1" bgcolor="#000000"><param name="allowFullScreen" value="true" /><param name="allowScriptAccess" value="always" /><param name="allowNetworking" value="all" /><param name="movie" value="http://www.twitch.tv/widgets/live_embed_player.swf" /><param name="flashvars" value="hostname=www.twitch.tv&channel=$1&auto_play=false&start_volume=50" /></object>';
+    var spreadsheet = {},
+        embed = '<iframe width="100%" height="450" frameborder="0" src="https://docs.google.com/spreadsheet/ccc?key=$1&usp=sharing&output=html&widget=true"></iframe>';
 
 
-    twitchtv.parse = function(postContent, callback) {
-        postContent = postContent.replace(/<a href="(?:http?:\/\/)?(?:www\.twitch\.tv)\/?(.+)">.+<\/a>/g, embed);
+    spreadsheet.parse = function(postContent, callback) {
+        postContent = postContent.replace(/<a href="(?:https?:\/\/)?(?:docs\.google\.com)\/spreadsheet\/ccc\?key=(.+)&usp=sharing">.+<\/a>/g, embed);
         callback(null, postContent);
     };
 
-    module.exports = twitchtv;
+    module.exports = spreadsheet;
 }(module));
